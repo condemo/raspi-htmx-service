bc380d130bd53747fb2ff03217c79be746504037
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-06 23:09:38 +0200

    added `TextResponse` utility

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/a-h/templ"
@@ -8,4 +9,9 @@ import (
 
 func RenderTempl(w http.ResponseWriter, r *http.Request, c templ.Component) {
 	c.Render(r.Context(), w)
+}
+
+func TextResponse(w http.ResponseWriter, msg string, status int) {
+	w.WriteHeader(status)
+	fmt.Fprint(w, msg)
 }
