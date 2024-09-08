914f366e55d19e92ce2729e50b116ad033573e43
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-08 16:58:45 +0200

    feat: custom handler with error management

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -2,10 +2,32 @@ package handlers
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/condemo/raspi-htmx-service/api/handlers/errors"
+	"github.com/condemo/raspi-htmx-service/public/views/components"
+	"github.com/condemo/raspi-htmx-service/public/views/core"
 )
+
+type customHandler func(w http.ResponseWriter, r *http.Request) error
+
+func MakeHandler(f customHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			switch err := err.(type) {
+			case errors.UINofifyError:
+				RenderTempl(w, r, components.SimpleError(err))
+			case errors.ApiError:
+				RenderTempl(w, r, core.ErrorPage(err))
+			default:
+				fmt.Fprintf(w, "unformat error -> %s", err.Error())
+			}
+			slog.Error("API ERROR", "err", err.Error(), "path", r.URL.Path)
+		}
+	}
+}
 
 func RenderTempl(w http.ResponseWriter, r *http.Request, c templ.Component) {
 	c.Render(r.Context(), w)
