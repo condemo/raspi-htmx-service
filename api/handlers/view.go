1c82f3ea4293570eb64ddd2176a7329b687f5219
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-08 19:09:23 +0200

    refactor: custom handler `homeView`

--- api/handlers/view.go
+++ api/handlers/view.go
@@ -13,9 +13,10 @@ func NewViewHandler() *ViewHandler {
 }
 
 func (h *ViewHandler) RegisterRoutes(r *http.ServeMux) {
-	r.HandleFunc("/", h.homeView)
+	r.HandleFunc("/", MakeHandler(h.homeView))
 }
 
-func (h *ViewHandler) homeView(w http.ResponseWriter, r *http.Request) {
+func (h *ViewHandler) homeView(w http.ResponseWriter, r *http.Request) error {
 	RenderTempl(w, r, core.Home())
+	return nil
 }
