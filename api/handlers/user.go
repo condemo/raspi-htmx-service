ef72ae30e1ed3e1e83039948b376c45b945e275a
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-12 23:06:33 +0200

    refactor: `auth.go` -> `user.go`

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/condemo/raspi-htmx-service/api/handlers/errors"
 	"github.com/condemo/raspi-htmx-service/api/utils"
@@ -23,6 +24,7 @@ func (h *AuthHandler) RegisterRoutes(r *http.ServeMux) {
 	r.HandleFunc("GET /login", MakeHandler(h.loginPage))
 	r.HandleFunc("POST /login", MakeHandler(h.login))
 	r.HandleFunc("POST /signup", MakeHandler(h.signup))
+	r.HandleFunc("POST /logout", MakeHandler(h.logout))
 }
 
 func (h *AuthHandler) loginPage(w http.ResponseWriter, r *http.Request) error {
@@ -49,14 +51,14 @@ func (h *AuthHandler) login(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	c := http.Cookie{
-		Name:    "token",
+		Name:    "raspi-token",
 		Value:   token,
 		Path:    "/",
 		Expires: expTime,
 	}
 
 	http.SetCookie(w, &c)
-	w.Header().Set("HX-Redirect", "/")
+	w.Header().Set("HX-Redirect", "/app")
 	return nil
 }
 
@@ -83,5 +85,19 @@ func (h *AuthHandler) signup(w http.ResponseWriter, r *http.Request) error {
 	w.Header().Add("HX-Redirect", "/auth/login")
 	w.WriteHeader(http.StatusCreated)
 
+	return nil
+}
+
+func (h *AuthHandler) logout(w http.ResponseWriter, r *http.Request) error {
+	c := http.Cookie{
+		Name:    "raspi-token",
+		Value:   "",
+		Path:    "/",
+		Expires: time.Unix(0, 0),
+	}
+
+	http.SetCookie(w, &c)
+	w.Header().Set("HX-Redirect", "/auth/login")
+	w.WriteHeader(http.StatusOK)
 	return nil
 }
