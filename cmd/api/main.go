4d4b575410ea8c7aa674f5712372d7d3684707d3
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-12 21:41:04 +0200

    feat: db setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/condemo/raspi-htmx-service/api"
+	"github.com/condemo/raspi-htmx-service/store"
 )
 
 func main() {
 	addr := flag.String("p", ":4000", "service port")
 	flag.Parse()
 
-	apiServer := api.NewApiServer(*addr)
+	sqlStorage := store.NewPostgresStore()
+	db, err := sqlStorage.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store := store.NewStorage(db)
+
+	apiServer := api.NewApiServer(*addr, store)
 	fmt.Println("Server Running on port", *addr)
 	apiServer.Run()
 }
