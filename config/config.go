7863a9cda7a07fb0c8e359b88f9ce9fa04603099
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-09-24 00:51:02 +0200

    added basic toml config setup

--- config/config.go
+++ config/config.go
@@ -1,9 +1,29 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+)
+
+// TODO: Replantear la estructura de la conf structs
+// para que faciliten las cosas con el archivo toml y
+// a la vez que sea lo más idóneo para enviar a las vistas
+
+var (
+	confDir  string = getConfDir()
+	confFile string = confDir + "/conf.toml"
+)
+
+type theme string
+
+const (
+	night     theme = "night"
+	cupcake   theme = "cupcake"
+	halloween theme = "halloween"
+	black     theme = "black"
+	sunset    theme = "sunset"
 )
 
 var Envs = initConfig()
@@ -25,4 +45,34 @@ func initConfig() envConfig {
 		DBName: os.Getenv("DB_NAME"),
 		DBPort: os.Getenv("DB_PORT"),
 	}
+}
+
+type uiConfig struct {
+	CurrentTheme theme `toml:"theme"`
+}
+
+func loadUIConfig() uiConfig {
+	return uiConfig{
+		CurrentTheme: night,
+	}
+}
+
+var UIConf = loadUIConfig()
+
+func getConfDir() string {
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	confDir = confDir + "/raspi"
+
+	if _, err := os.Stat(confDir); os.IsNotExist(err) {
+		err := os.Mkdir(confDir, os.FileMode(0o744))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return confDir
 }
---
+++ config/persistent.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+func SaveConf() {
+	f, err := os.Create(confFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	err = toml.NewEncoder(f).Encode(UIConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Config Saved")
+}
