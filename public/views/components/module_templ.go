d80f68b138822ebacb7b9b0e110e18c2e042ece6
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-10-05 19:43:54 +0200

    ref(ui): min tweaks

--- public/views/components/module_templ.go
+++ public/views/components/module_templ.go
@@ -35,7 +35,7 @@ func ServiceCarousel() templ.Component {
 			return templ_7745c5c3_Err
 		}
 		for i := range 8 {
-			templ_7745c5c3_Err = ServiceCard(fmt.Sprintf("service-%d", i)).Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = ServiceCard(fmt.Sprintf("service-%d", i+1)).Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -79,7 +79,20 @@ func ServiceCard(name string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div class=\"carousel-item\"><div class=\"card bg-base-100 w-72 shadow-xl\"><figure class=\" h-10 rounded-3xl m-1 bg-red-700\"><!-- <img --><!-- \tsrc=\"https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp\" --><!-- \talt=\"Shoes\" --><!-- \tclass=\"rounded-xl h-14 w-full m-1\" --><!-- /> --></figure><div class=\"card-body items-center text-center\"><h2 class=\"card-title\">Shoes!</h2><p>If a dog chews shoes whose shoes does he choose?</p><div class=\"card-actions\"><button class=\"btn btn-primary\">Buy Now</button></div></div></div></div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div class=\"carousel-item\"><div class=\"card bg-base-100 w-72 shadow-xl\"><figure class=\" h-10 rounded-3xl m-1 bg-base-300\"><h1 class=\"z-10\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 string
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(name)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `public/views/components/module.templ`, Line: 22, Col: 28}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h1></figure><div class=\"card-body items-center text-center\"><h2 class=\"card-title\">[OFF]</h2><div id=\"card-body\"><p>If a dog chews shoes whose shoes does he choose?</p><p>If a dog chews shoes whose shoes does he choose?</p></div><div class=\"card-actions w-full flex justify-between\"><button class=\"btn btn-accent w-36\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"size-6\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z\"></path></svg></button> <button class=\"btn bg-base-200 w-31\"><svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"size-6\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z\"></path> <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z\"></path></svg></button></div></div></div></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
