dc923d65bbf9682037d9c9aa3650e59684374496
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-11-04 01:30:20 +0100

    ref: port :8080 -> :8000

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -3,6 +3,6 @@ package main
 import "github.com/condemo/raspi-htmx-service/services/manager"
 
 func main() {
-	grpcServer := manager.NewGrpcServer(":8080")
+	grpcServer := manager.NewGrpcServer(":8000")
 	grpcServer.Run()
 }
