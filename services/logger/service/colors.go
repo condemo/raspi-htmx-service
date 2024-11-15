8bc5630459decb5b5637a956a4fff057c7de8485
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-11-15 01:16:42 +0100

    basic color consts for stdout

---
+++ services/logger/service/colors.go
@@ -0,0 +1,9 @@
+package service
+
+const (
+	COLOR_INFO    = "\u001b[34m"
+	COLOR_SUCCESS = "\u001b[32m"
+	COLOR_WARNING = "\u001b[33m"
+	COLOR_ERROR   = "\u001b[31m"
+	COLOR_NONE    = "\u001b[0m"
+)
