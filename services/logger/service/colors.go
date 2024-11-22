a3dc08f23facb230eaeac8dd0f49053cef9f2d01
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-11-22 19:57:38 +0100

    ref: new term logs colors

--- services/logger/service/colors.go
+++ services/logger/service/colors.go
@@ -1,9 +1,9 @@
 package service
 
 const (
-	COLOR_INFO    = "\u001b[34m"
-	COLOR_SUCCESS = "\u001b[32m"
-	COLOR_WARNING = "\u001b[33m"
-	COLOR_ERROR   = "\u001b[31m"
+	COLOR_INFO    = "\033[38;5;39m"
+	COLOR_SUCCESS = "\033[38;5;42m"
+	COLOR_WARNING = "\033[38;5;221m"
+	COLOR_ERROR   = "\033[38;5;160m"
 	COLOR_NONE    = "\u001b[0m"
 )
