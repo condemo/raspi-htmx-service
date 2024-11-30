22ccab7706f4bb065d73320ff8772e9d21433ccc
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-11-30 20:19:26 +0100

    feat: added `/full/{id}` route and handler

--- services/web/api/handlers/services.go
+++ services/web/api/handlers/services.go
@@ -25,6 +25,7 @@ func NewServiceHandler(mConn *grpc.ClientConn) *ServiceHandler {
 func (h *ServiceHandler) RegisterRoutes(r *http.ServeMux) {
 	r.HandleFunc("POST /start/{id}", MakeHandler(h.startService))
 	r.HandleFunc("POST /stop/{id}", MakeHandler(h.stopService))
+	r.HandleFunc("GET /full/{id}", MakeHandler(h.getFullInfo))
 }
 
 func (h *ServiceHandler) startService(w http.ResponseWriter, r *http.Request) error {
@@ -59,4 +60,21 @@ func (h *ServiceHandler) stopService(w http.ResponseWriter, r *http.Request) err
 	}
 
 	return RenderTempl(w, r, components.ServiceCard(serv))
+}
+
+func (h *ServiceHandler) getFullInfo(w http.ResponseWriter, r *http.Request) error {
+	id, err := strconv.ParseInt(r.PathValue("id"), 10, 32)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*2)
+	defer cancel()
+
+	res, err := h.managerConn.GetFullInfo(ctx, &manager.ServiceIdRequest{Id: int32(id)})
+	if err != nil {
+		return err
+	}
+
+	return RenderTempl(w, r, components.FullInfoView(res))
 }
