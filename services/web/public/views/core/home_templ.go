be53799d6794b3d7191821ce40283c7545464967
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-11-30 20:20:00 +0100

    feat: added full info div

--- services/web/public/views/core/home_templ.go
+++ services/web/public/views/core/home_templ.go
@@ -61,7 +61,7 @@ func Home(i *sysinfo.SysInfo, services []*manager.RaspiService) templ.Component 
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"service-full-info\"></div></div></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
