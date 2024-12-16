5d30166a838d9c0c818e4997ddb03e91e2f53ae2
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-12-16 20:32:59 +0100

    feat: error logs are persistent now

---
+++ services/logger/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type LoggerConfig struct {
+	LogFilePath string
+}
+
+func NewLoggerConfig() *LoggerConfig {
+	lc := &LoggerConfig{}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		// TODO: Mejorar error handling
+		log.Fatal(err)
+	}
+
+	logDir := filepath.Join(homeDir, "/.local/share/raspi")
+	logFile := filepath.Join(logDir, "errors")
+
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		err := os.Mkdir(logDir, os.FileMode(0o744))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		f, err := os.Create(logFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+	}
+
+	lc.LogFilePath = logFile
+
+	return lc
+}
+
+func (c *LoggerConfig) SaveLog(msg string) error {
+	f, err := os.OpenFile(c.LogFilePath, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(msg + "\n"); err != nil {
+		return err
+	}
+
+	return nil
+}
--- services/logger/service/service.go
+++ services/logger/service/service.go
@@ -6,32 +6,39 @@ import (
 	"time"
 
 	"github.com/condemo/raspi-htmx-service/services/common/genproto/pb"
+	"github.com/condemo/raspi-htmx-service/services/logger/config"
 )
 
 type LoggerService struct {
-	// injects..
+	conf *config.LoggerConfig
 }
 
 func NewLoggerService() *LoggerService {
-	return &LoggerService{}
+	return &LoggerService{
+		conf: config.NewLoggerConfig(),
+	}
 }
 
 func (s *LoggerService) LogMessage(ctx context.Context, req *pb.LogRequest) error {
-	// PERF: Podría user un `strings.Builder` para ser mas eficiente
+	// PERF: Podría usar un `strings.Builder` para ser mas eficiente
 	var color string
+	currentTime := time.Now().Format("01/02/2006 15:04:05")
 
 	switch req.GetType() {
 	case pb.LogMessageType_INFO:
 		color = COLOR_INFO
 	case pb.LogMessageType_ERROR:
 		color = COLOR_ERROR
+		msg := fmt.Sprintf("%s [%s]: %s", currentTime, req.GetServiceName(), req.GetMsg())
+		if err := s.conf.SaveLog(msg); err != nil {
+			return err
+		}
 	case pb.LogMessageType_SUCCESS:
 		color = COLOR_SUCCESS
 	case pb.LogMessageType_WARNING:
 		color = COLOR_WARNING
 	}
 
-	currentTime := time.Now().Format("01/02/2006 15:04:05")
 	fmt.Printf("%s%s [%s%s%s] %s%s\n", color, currentTime, COLOR_NONE, req.GetServiceName(), color, req.GetMsg(), COLOR_NONE)
 
 	return nil
