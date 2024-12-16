0ab834d0374f0f8f949d3de033549d44cb3fc331
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-12-16 21:48:31 +0100

    feat: added `CleanErrorLog` with `CleanErrorLogReq` and `CleanErrorLogRes`

--- services/common/genproto/pb/logger_grpc.pb.go
+++ services/common/genproto/pb/logger_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	LoggerService_LogMessage_FullMethodName = "/logger.LoggerService/LogMessage"
+	LoggerService_LogMessage_FullMethodName    = "/logger.LoggerService/LogMessage"
+	LoggerService_CleanErrorLog_FullMethodName = "/logger.LoggerService/CleanErrorLog"
 )
 
 // LoggerServiceClient is the client API for LoggerService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type LoggerServiceClient interface {
 	LogMessage(ctx context.Context, in *LogRequest, opts ...grpc.CallOption) (*LogResponse, error)
+	CleanErrorLog(ctx context.Context, in *CleanErrorReq, opts ...grpc.CallOption) (*CleanErrorRes, error)
 }
 
 type loggerServiceClient struct {
@@ -47,11 +49,22 @@ func (c *loggerServiceClient) LogMessage(ctx context.Context, in *LogRequest, op
 	return out, nil
 }
 
+func (c *loggerServiceClient) CleanErrorLog(ctx context.Context, in *CleanErrorReq, opts ...grpc.CallOption) (*CleanErrorRes, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(CleanErrorRes)
+	err := c.cc.Invoke(ctx, LoggerService_CleanErrorLog_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // LoggerServiceServer is the server API for LoggerService service.
 // All implementations must embed UnimplementedLoggerServiceServer
 // for forward compatibility.
 type LoggerServiceServer interface {
 	LogMessage(context.Context, *LogRequest) (*LogResponse, error)
+	CleanErrorLog(context.Context, *CleanErrorReq) (*CleanErrorRes, error)
 	mustEmbedUnimplementedLoggerServiceServer()
 }
 
@@ -64,6 +77,9 @@ type UnimplementedLoggerServiceServer struct{}
 
 func (UnimplementedLoggerServiceServer) LogMessage(context.Context, *LogRequest) (*LogResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LogMessage not implemented")
+}
+func (UnimplementedLoggerServiceServer) CleanErrorLog(context.Context, *CleanErrorReq) (*CleanErrorRes, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CleanErrorLog not implemented")
 }
 func (UnimplementedLoggerServiceServer) mustEmbedUnimplementedLoggerServiceServer() {}
 func (UnimplementedLoggerServiceServer) testEmbeddedByValue()                       {}
@@ -104,6 +120,24 @@ func _LoggerService_LogMessage_Handler(srv interface{}, ctx context.Context, dec
 	return interceptor(ctx, in, info, handler)
 }
 
+func _LoggerService_CleanErrorLog_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CleanErrorReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(LoggerServiceServer).CleanErrorLog(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: LoggerService_CleanErrorLog_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(LoggerServiceServer).CleanErrorLog(ctx, req.(*CleanErrorReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // LoggerService_ServiceDesc is the grpc.ServiceDesc for LoggerService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +148,10 @@ var LoggerService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LogMessage",
 			Handler:    _LoggerService_LogMessage_Handler,
+		},
+		{
+			MethodName: "CleanErrorLog",
+			Handler:    _LoggerService_CleanErrorLog_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
